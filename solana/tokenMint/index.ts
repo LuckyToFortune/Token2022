ab5e3f024295bf219ced7fd3d49468cc0e9cd71e
LuckyToFortune <yusuke.ori@gmail.com>
2024-08-22 22:42:22 -0600

    fixed index.ts

--- solana/tokenMint/index.ts
+++ solana/tokenMint/index.ts
@@ -28,27 +28,20 @@ export const mintToken = async (sessionInfo: SessionInfo) => {
       "https://salmon-occupational-spider-382.mypinata.cloud/ipfs/QmTySyzK269AcwHkFtKBWX6aSG1qpUitDmHco7CuqATkaG";
 
     // 2. Create Mint with Transfer Fee
-    // const tokenAccount = await createMintWithTransferFee(
-    //   sessionInfo,
-    //   metadataUrl
-    // );
-    // return tokenAccount.toString();
-
-    // // 4. Transfer Tokens with Fee
-    // const amount: bigint = BigInt(10);
-    // await transferTokensWithFee(mint, tokenAccount, sessionInfo, amount);
-
-    // // 5. Withdraw Withheld Fees
-    const mint = new PublicKey("2SjvXzrucowqvPy29N4DRj9jhH1miQMQZeAJMMTw6jDk");
-    await withdrawWithheldFees(mint, sessionInfo);
-
-    // console.log(
-    //   "Successfully minted tokens with transfer fee (Mint:",
-    //   mint.toString(),
-    //   ")"
-    // );
+    const tokenAccount = await createMintWithTransferFee(
+      sessionInfo,
+      metadataUrl
+    );
+    return tokenAccount.toString();
   } catch (error) {
     console.error("Error minting tokens:", error);
     return null;
   }
 };
+
+// Transfer Tokens with Fee
+// const amount: bigint = BigInt(10);
+// await transferTokensWithFee(mint, tokenAccount, sessionInfo, amount);
+
+// Withdraw Withheld Fees
+// await withdrawWithheldFees(mint, sessionInfo);
